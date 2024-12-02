1b330f8796d7b2a4e56ae020cbc8109a79d0da07
Viktor Frideen <frideen.viktor@gmail.com>
2024-12-02 22:56:59 +0100

    Initial commit

---
+++ u/u.py
@@ -0,0 +1,38 @@
+import re
+
+
+def read_grid(filename, dataparser):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+        d = list()
+
+        for line in lines:
+            rd = list()
+            for col in line:
+                v = dataparser(col)
+                if v is None:
+                    continue
+
+                rd.append(v)
+
+            if len(rd) > 0:
+                d.append(rd)
+
+        return d
+
+
+def read_ssv(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+        d = list()
+
+        for line in lines:
+            rd = re.split(r"\s+", line.strip())
+            rd = [int(r) for r in rd]
+
+            if len(rd) > 0:
+                d.append(rd)
+
+        return d
