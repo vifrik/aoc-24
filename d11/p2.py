eda9741bb7cedc78507814576e0b4368e3d21a7e
Viktor Frideen <frideen.viktor@gmail.com>
2024-12-11 18:49:17 +0100

    Add day 11

---
+++ d11/p2.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+with open("i.txt", "r") as f:
+    content = f.read().strip()
+    stones = [int(s) for s in content.split()]
+    sc = Counter(stones)
+
+for _ in range(75):
+    new_sc = Counter()
+    for s, c in sc.items():
+        ss = str(s)
+        if s == 0:
+            new_sc[1] += c
+        elif len(ss) % 2 == 0:
+            s1, s2 = int(ss[len(ss) // 2 :]), int(ss[: len(ss) // 2])
+            new_sc[s1] += c
+            new_sc[s2] += c
+        else:
+            new_sc[s * 2024] += c
+    sc = new_sc
+
+print(sum(sc.values()))
